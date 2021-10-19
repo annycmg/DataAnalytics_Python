d85b67162adc1a716bd654570e7d49e14be37116
annycmg <38812492+annycmg@users.noreply.github.com>
2021-10-19 14:28:27 +0200

    Update SQL_Statement.py

--- SQL_Statement.py
+++ SQL_Statement.py
@@ -11,7 +11,7 @@ cursor = conn.cursor()
 
 query = cursor.execute('SELECT * FROM table01')
 
-query2 = cursor.execute("INSERT INTO table01 (Name, Age, StandardCost, ListPrice, SellStartDate) \
+cursor.execute("INSERT INTO table01 (Name, Age, StandardCost, ListPrice, SellStartDate) \
                         VALUES ('?',?,?,?,?)", 'Anny', 26, 3175, 3, '2021-10-19')
 conn.commit()
 conn.close()
@@ -22,7 +22,7 @@ import pymssql
 conn2 = pymssql.connect(server='yourserver.database.windows.net', user='yourusername@yourserver', password='yourpassword', database='AdventureWorks')  
 cursor = conn2.cursor()  
 
-query3 = cursor.execute("INSERT INTO table01 (Name, Age, StandardCost, ListPrice, SellStartDate) \ 
+cursor.execute("INSERT INTO table01 (Name, Age, StandardCost, ListPrice, SellStartDate) \ 
                          VALUES ('Anny', 26, 3175, 3, '2021-10-19')")  
 conn2.commit()
 conn2.close()
